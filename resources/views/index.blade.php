b25eedf6346f742f6697f56927aaeddcb7b26863
frank jones <frankjones.web@gmail.com>
2022-08-05 16:59:40 +0100

    Basic layout and CSS

---
+++ resources/views/index.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Curated Design</title>
+    <link rel="stylesheet" href="{{asset('css/custom.css')}}">
+</head>
+<body>
+
+    <header>
+        <div class="container">
+            <div class="logo">
+                Curated Design
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Ceramics</a>
+                    </li>
+                    <li>
+                        <a href="#">Clocks</a>
+                    </li>
+                    <li>
+                        <a href="#">Lighting</a>
+                    </li>
+                    <li>
+                        <a href="#">Radios</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            This is the main content area of the page.
+        </div>
+    </main>
+
+    <footer>
+        <div class="container">
+            <div class="copyright">Curated Design &copy; {{date('Y')}} | All rights reserved.</div>
+            <div class="links">
+                <ul>
+                    <li>
+                        <a href="#">Privacy policy</a>
+                    </li>
+                    <li>
+                        <a href="#">Returns  policy</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+    
+</body>
+</html>
